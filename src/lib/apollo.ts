ff0ea7a3437b67b1d4c418188bd166cea16eb305
Gustavo <gustavosilva419@gmail.com>
2022-06-26 21:46:56 -0300

    Aula-4

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4tf73mt029a01ukc01hbt9g/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Auhorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
